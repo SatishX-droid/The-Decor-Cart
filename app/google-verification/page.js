b83d678026ffcd3f1d2ba4fd360bc91d3712485b
SatishX-Droid <dineshdhull339@gmail.com>
2026-09-20 11:36:13 +0530

    Add Google verification route

---
+++ app/google-verification/page.js
@@ -0,0 +1,9 @@
+import { redirect } from 'next/navigation';
+
+export default function GoogleVerification() {
+  return (
+    <pre style={{ whiteSpace: 'pre-wrap' }}>
+      google-site-verification: google4de67fec486f31de.html
+    </pre>
+  );
+}
